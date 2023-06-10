0da07acde14080f776421845d37c002656fc45fd
DESKTOP-LTULUJU\ASUS <chj990707@postech.ac.kr>
2023-06-11 05:11:57 +0900

    Removed unnecessary imports from Marauder

--- src/main/java/com/bunting/bannersandtrumpets/entities/MarauderEntity.java
+++ src/main/java/com/bunting/bannersandtrumpets/entities/MarauderEntity.java
@@ -3,20 +3,11 @@ package com.bunting.bannersandtrumpets.entities;
 import com.bunting.bannersandtrumpets.EntityRegistry;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.entity.*;
-import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.control.MoveControl;
-import net.minecraft.world.entity.ai.goal.RangedCrossbowAttackGoal;
-import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.animal.horse.Horse;
-import net.minecraft.world.entity.monster.AbstractIllager;
-import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.monster.Pillager;
-import net.minecraft.world.entity.monster.Vindicator;
-import net.minecraft.world.entity.raid.Raider;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
 
@@ -57,10 +48,5 @@ public class MarauderEntity extends Pillager {
         horse.equipSaddle(null);
         this.startRiding(horse);
         return p_33793_;
-    }
-
-    @Override
-    public SoundEvent getCelebrateSound() {
-        return null;
     }
 }
